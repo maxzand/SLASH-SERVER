b9d692cecf14c0ff3e14d14c2bee3ba606f3aab3
snoopalpaca <ameernessaee9536@Gmail.com>
2021-06-03 14:55:15 -0700

    ADD Clock Sync
    Added FetchServerTime() and
    DetermineLatency() to Server.gd

--- GameServer/Server.gd
+++ GameServer/Server.gd
@@ -24,6 +24,16 @@ func _Peer_Disconnected(player_id):
 		rpc_id(0, "DespawnPlayer", player_id)
 		player_state_collection.erase(player_id)
 
+
+remote func FetchServerTime(client_time):
+	var player_id = get_tree().get_rpc_sender_id()
+	rpc_id(player_id, "ReturnServerTime", OS.get_system_time_msecs(), client_time)
+	
+remote func DetermineLatency(client_time):
+	var player_id = get_tree().get_rpc_sender_id()
+	rpc_id(player_id, "ReturnLatency", client_time)
+
+
 remote func ReceivePlayerState(player_state):
 	var player_id = get_tree().get_rpc_sender_id()
 	if player_state_collection.has(player_id):
